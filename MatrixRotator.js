08e5c2434a70caf23b2fcb981f8a1787ececfa48
DLeaguer <immostlikely@hotmail.com>
2018-07-15 18:47:44 -1000

    redid and added console.logs to show on npm test

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -19,6 +19,63 @@ module.exports = class MatrixRotator {
   //      v        or Direction.CCW
   rotate (direction) {
     // do work here
+    
+    console.log('');
+    console.log('direction ---> ', direction);
+    console.log('   *** this.matrix *** ');
+    console.log(this.matrix);
+
+    let theMatrix = this.matrix;
+    let stageMatrix = [];
+    let rotatedMatrix = [];
+
+    if (direction === 'ClockWise') {
+      while (theMatrix[0].length > 0) {
+        console.log('');
+        console.log('theMatrix[0].length ---> ', theMatrix[0].length + '  in each array!');
+        for (let i = theMatrix.length-1; i >= 0; i--) {
+          stageMatrix.push(theMatrix[i][0]);
+          console.log('theMatrix[i][0]    ', theMatrix[i][0]);
+          console.log('stageMatrix    ', stageMatrix);
+          theMatrix[i].shift();
+          console.log(' >>> theMatrix <<<');
+          console.log(theMatrix);
+        }
+        rotatedMatrix.push(stageMatrix);
+        console.log(' ^^^ stageMatrix ^^^');
+        console.log(stageMatrix);
+        stageMatrix = [];
+        console.log('stageMatrix reset ---> ', stageMatrix);
+      }
+      this.matrix = rotatedMatrix;
+      console.log('   $$$ this.matrix $$$');
+      console.log(this.matrix);
+      console.log('');
+    }
+
+    else if (direction === 'CounterClockWise') {
+      while (theMatrix[0].length > 0) {
+        console.log('');
+        console.log('theMatrix[0].length ---> ', theMatrix[0].length + '  in each array!');
+        for (let i = 0; i < theMatrix.length; i++) {
+          stageMatrix.push(theMatrix[i][theMatrix[i].length-1]);
+          console.log('theMatrix[i][theMatrix[i].length-1] ---> ', theMatrix[i][theMatrix[i].length-1]);
+          console.log('stageMatrix    ', stageMatrix);
+          theMatrix[i].pop();
+          console.log(' >>> theMatrix <<<');
+          console.log(theMatrix);
+        }
+        rotatedMatrix.push(stageMatrix);
+        console.log(' ^^^ stageMatrix ^^^');
+        console.log(stageMatrix);
+        stageMatrix = [];
+        console.log('stageMatrix reset ---> ', stageMatrix);
+      }
+      this.matrix = rotatedMatrix;
+      console.log('   $$$ this.matrix $$$');
+      console.log(this.matrix);
+      console.log('');
+    }
 
     // must be a valid Direction, see Direction.js
 
